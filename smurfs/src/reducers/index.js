54d08bcdfcbb912a3d942f2bf70853b013380cae
Samsoucy <newf930@outlook.com>
2019-02-08 11:55:44 -0500

    imported action types, added initialState and added reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,12 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+import {
+  FETCH_SMURF_START,
+  FETCH_SMURF_SUCCESS,
+  FETCH_SMURF_FAILURE
+} from "../actions";
+
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -14,6 +20,15 @@
  }
 */
 
+const initialState = {
+   smurfs: [],
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
+   error: null
+}
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +36,27 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_SMURF_START:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+    case FETCH_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false
+      };
+    case FETCH_SMURF_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        fetchingSmurfs: false
+      }
+  }
+}
+
+export default reducer;
